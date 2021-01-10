e9762e586fca4bf227dcc3815aaefc48ad86fce1
FSanchir <64943969+fsanchir@users.noreply.github.com>
2021-01-10 22:17:41 +0900

    Update Main.java

--- src/team/fi/slfa/Main.java
+++ src/team/fi/slfa/Main.java
@@ -9,29 +9,52 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 	ConsoleCommandSender console = Bukkit.getConsoleSender();
+	private static Main instance;
+	public static Main getInstance() {
+		return instance;
+	}
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent e) {
+		//Bukkit.broadcastMessage("§c§lTEST");
+		Player player = e.getEntity();
+		if(player instanceof Player) {
+			Player player1 = e.getEntity().getPlayer();
+			e.setDeathMessage("§cPlayer §l"+player1.getName()+"§c Died!");
+			player1.getLocation().getWorld().playEffect(player1.getLocation(), Effect.MOBSPAWNER_FLAMES, 20);
+			if(player1.hasPermission("slpa.bypass") == false) {
+				Bukkit.getBanList(Type.NAME).addBan(player1.getName(), "§c§lYou died!", null, "console");
+				player1.kickPlayer("§c§lYou have Died!");
+			}
+		}
+		else {
+			return;
+		}
+
+	}
+	@EventHandler
+	public void join(PlayerJoinEvent e) {
+		String plnick = e.getPlayer().getName();
+		e.setJoinMessage(ChatColor.GREEN+"[+]"+plnick+ChatColor.GOLD+"님이 접속하셨습니다!");
+	}
+	@EventHandler
+	public void leave(PlayerQuitEvent e) {
+		String plnick = e.getPlayer().getName();
+		e.setQuitMessage(ChatColor.GREEN+"["+ChatColor.DARK_RED+"-"+ChatColor.GREEN+"]"+plnick+ChatColor.GOLD+"님이 퇴장하셨습니다!");	
+	}
 	@Override
 	public void onEnable() {
 		console.sendMessage( ChatColor.AQUA + "SLPA Enabling");
+		Main.instance = this;
+		getServer().getPluginManager().registerEvents(this, this);
 	}
 	@Override
 	public void onDisable() {
 		console.sendMessage( ChatColor.AQUA + "SLPA Disabling");
-	}
-	@EventHandler()
-	public void onPlayerDeath(PlayerDeathEvent event) {
-		
-		Player player = event.getEntity();
-		player.getLocation().getWorld().playEffect(player.getLocation(), Effect.SMOKE, 1);
-		if(player.hasPermission("slpa.bypass") == false) {
-			Bukkit.getBanList(Type.NAME).addBan(player.getName(), "§c§lYou died!", null, "console");
-			player.kickPlayer("§c§lYou have Died!");
-			//Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "kick "+player.getName());
-		}
-		event.setDeathMessage("§cPlayer §l"+player.getName()+"§c is Died!");
-	//Bukkit.getBanlist(Type.NAME).addBan(player.getName(), "You died!", expires, "console");
 	}
 }
