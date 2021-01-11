ce99ee02a48bff505bbe987fdc340e3cd48cf07b
FSanchir <64943969+fsanchir@users.noreply.github.com>
2021-01-11 21:47:53 +0900

    Update Main.java

--- src/team/fi/slfa/Main.java
+++ src/team/fi/slfa/Main.java
@@ -1,9 +1,15 @@
 package team.fi.slfa;
+import java.util.ArrayList;
+import team.fi.slfa.Cmd;
 // 혹시 읽는 분 계시면 패키지 명 무시해주세요.. 귀찮아서 For 넣었다가 Per로 바꿈..
 import org.bukkit.BanList.Type;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+//import org.bukkit.World;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,9 +17,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
+	private static Plugin plugin;
 	ConsoleCommandSender console = Bukkit.getConsoleSender();
 	private static Main instance;
 	public static Main getInstance() {
@@ -26,11 +36,24 @@ public class Main extends JavaPlugin implements Listener {
 		if(player instanceof Player) {
 			Player player1 = e.getEntity().getPlayer();
 			e.setDeathMessage("§cPlayer §l"+player1.getName()+"§c Died!");
-			player1.getLocation().getWorld().playEffect(player1.getLocation(), Effect.MOBSPAWNER_FLAMES, 20);
+			World playerworld = player1.getLocation().getWorld();
+			Location playerlocation = player1.getLocation();
+			playerworld.playEffect(playerlocation, Effect.MOBSPAWNER_FLAMES, 20);
+			Bukkit.getScheduler ().runTaskLater (this, () -> playerworld.playEffect(playerlocation, Effect.MOBSPAWNER_FLAMES, 20), 20);
 			if(player1.hasPermission("slpa.bypass") == false) {
 				Bukkit.getBanList(Type.NAME).addBan(player1.getName(), "§c§lYou died!", null, "console");
 				player1.kickPlayer("§c§lYou have Died!");
 			}
+			
+			ItemStack item = new ItemStack(Material.PLAYER_HEAD, 1);
+			SkullMeta skull = (SkullMeta) item.getItemMeta();
+			skull.setDisplayName(player1.getName());
+			ArrayList<String> lore = new ArrayList<String>();
+			lore.add("§6§lThis head's owner is dead.");
+			skull.setLore(lore);
+			skull.setOwningPlayer(player1);
+			item.setItemMeta(skull);
+			player1.getWorld().dropItem(player.getLocation().add(0,1,0), item);
 		}
 		else {
 			return;
@@ -49,12 +72,20 @@ public class Main extends JavaPlugin implements Listener {
 	}
 	@Override
 	public void onEnable() {
+		plugin = this;
 		console.sendMessage( ChatColor.AQUA + "SLPA Enabling");
 		Main.instance = this;
 		getServer().getPluginManager().registerEvents(this, this);
+		com();
 	}
 	@Override
 	public void onDisable() {
 		console.sendMessage( ChatColor.AQUA + "SLPA Disabling");
+	}
+	public static Plugin getpl() {
+		return plugin;
+	}
+	public void com() {
+		getCommand("slpa").setExecutor(new Cmd());
 	}
 }
