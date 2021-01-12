3c5ea980b4a719c170b95ce3c9baa6c4892ff477
FSanchir <64943969+fsanchir@users.noreply.github.com>
2021-01-12 13:27:24 +0900

    Update Cmd.java

--- src/team/fi/slfa/Cmd.java
+++ src/team/fi/slfa/Cmd.java
@@ -5,13 +5,13 @@ import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
+//import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
+//import org.bukkit.plugin.Plugin;
+//import org.bukkit.plugin.java.JavaPlugin;
 
-import team.fi.slfa.Main;
+//import team.fi.slfa.Main;
 
 public class Cmd implements CommandExecutor {
 	@Override
